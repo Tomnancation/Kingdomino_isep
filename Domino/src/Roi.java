b76374e67797780d4bb0c268183d9309cdcc9512
jiajin Tang <63906292+Tomnancation@users.noreply.github.com>
2021-06-13 17:54:48 +0200

    13/05/2021

--- Domino/src/Roi.java
+++ Domino/src/Roi.java
@@ -4,7 +4,7 @@ import org.newdawn.slick.*;
 
 import org.newdawn.slick.*;
 
-//1.为什么要定义ID_Roi=1
+
 public class Roi {
 	private int id;
 	private String couleur;
@@ -34,15 +34,11 @@ public class Roi {
 	public void setCouleur(String couleur) {
 		this.couleur = couleur;
 	}
-	/**
-	 *1.printDominoList，dominoListDraw，getJoueurByRoi都是关于console
-	 *2.dominoOk是什么东西 
-	 *3.59行有关于AI的代码*/
+	
 	public Domino draw() {
 		Domino d = null;
 		Console.printDominoList(Console.dominoListDraw);
 		boolean dominoOk = false;
-		if (Console.getJoueurByRoi(this).joueurType.equals("Person")) {
 			do {
 				try {
 					Console.printRoiInfo(this);
@@ -51,31 +47,14 @@ public class Roi {
 					d = Console.dominoListDraw.get(n);  // on prend un domino
 					Console.dominoListDraw.remove(d);  // on retire un domino de la pioche
 					dominoOk = true;
+					
 				} catch (Exception e) {
 					System.out.println("Erreur.");
 					// e.printStackTrace();
 				}
 			} while (!dominoOk);
-		}  else { // AI
-			 if (Console.tour == 1) {
-				 int n = Console.getJoueurByRoi(this).chooseBestDominoTurn1(Console.dominoListDraw);
-				 d = Console.dominoListDraw.get(n);
-				 Console.dominoListDraw.remove(d);
-			 } else {
-				System.out.println("other rounds");
-				Domino pre = Console.RoiToDomino.get(this);
-				int x = Console.getJoueurByRoi(this).bestPosition(pre)[1];
-				int y = Console.getJoueurByRoi(this).bestPosition(pre)[2];
-				int direction = Console.getJoueurByRoi(this).bestPosition(pre)[3];
-				int n = Console.getJoueurByRoi(this).chooseBestDomino(pre, x, y, direction, Console.dominoListDraw);
-				d = Console.dominoListDraw.get(n);
-				Console.dominoListDraw.remove(d);
-				System.out.println("AI chose the domino " + d.getNumDomino());
-			 }
-		}
-		return d;
-
-			//}
+			return d;
+			
 		}
 	public Color color() {
 		switch (couleur) {
